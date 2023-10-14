e93ded218e6c3e6da46e0eeecd06b1a5815d79c3
matheusoaresilva <matheusmatias2622@gmail.com>
2023-10-13 21:37:39 -0300

    feat: adiciona tela criacao de user

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RedefinirSenhaUsuarioComponent } from './pages/redefinir-senha-usuario/redefinir-senha-usuario.component';
+import { CadastroUsuarioComponent } from './pages/cadastro-usuario/cadastro-usuario.component';
 
 const routes: Routes = [
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path:'password_reset',
     component: RedefinirSenhaUsuarioComponent
+  },
+  {
+    path: 'register_account',
+    component: CadastroUsuarioComponent
   }
 ];
 
